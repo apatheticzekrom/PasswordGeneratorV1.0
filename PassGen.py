86bc3c37dfa0b758e478747ff696aef7868ff972
Benjamin Giang <apatheticzekrom@gmail.com>
2022-04-29 01:39:42 -0400

    fixed while loops in inputs, added includeSymbols, added return to checkYesNo()

--- PassGen.py
+++ PassGen.py
@@ -1,37 +1,63 @@
-
-
 def menu():
     print("[1] Password Generator")
     print("[2] Ping")
     print("[0] Option 0")
 
-# def inputChecker()
+# Function to return True/False based on (y/n) inputs
+def checkYesNo(answer):
+    if answer == "y":
+        return True
+    else:
+        return False
+
 
 # [1] Password Generator
 def option1():
-    answer = str
-    passLength = int
     
+    # Password Length
     answer = str(input("Enter password length: "))
-
     while answer.isdigit() != True:
         answer = str(input("Invalid. Enter an integer for password length: "))
     
     passLength = int(answer)
+    
+    
+    
+    # Include Lowercase
+    answer = str(input("Include lowercase? (y/n): "))
+    while answer != "y" and answer != "n":
+        answer = str(input("Invalid. Enter (y/n): "))
+    
+    includeLower = checkYesNo(answer)
+    
+    
+    # Include Uppercase
+    answer = str(input("Include uppercase? (y/n): "))
+    while answer != "y" and answer != "n":
+        answer = str(input("Invalid. Enter (y/n): "))
+    
+    includeUpper = checkYesNo(answer)
+        
+        
+    # Include Numbers
+    answer = str(input("Include numbers? (y/n): "))
+    while answer != "y" and answer != "n":
+        answer = str(input("Invalid. Enter (y/n): "))
+    
+    includeNumbers = checkYesNo(answer)
+    
+    # Include Symbols
+    answer = str(input("Include symbols? (y/n): "))
+    while answer != "y" and answer != "n":
+        answer = str(input("Invalid. Enter (y/n): "))
+    
+    includeSymbols = checkYesNo(answer)
+ 
     print(passLength)
-    
-    # includeLower = int(input("Include lowercase? (y/n): "))
-    
-    # includeUpper = int(input("Include uppercase? (y/n): "))
-    
-    # includeNumbers = int(input("Include numbers? (y/n): "))
-    
-    # includeSymbols = int(input("Include symbols? (y/n) "))
-    
-    
-    
-    
-    
+    print(includeLower)
+    print(includeUpper)
+    print(includeNumbers)
+    print(includeSymbols)
 
 # [2] Ping
 def option2():
